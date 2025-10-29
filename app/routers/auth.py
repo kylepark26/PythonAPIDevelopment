ee3c03b5957b416329860a53b3a59c2a702f6789
Kyle Park <park.3437@osu.edu>
2025-10-28 20:22:11 -0400

    fixed routers and oauth2

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,7 +5,7 @@ from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
-@router.post('/login')
+@router.post('/login', response_model=schemas.Token)
 async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
@@ -18,7 +18,7 @@ async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Ses
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
 
     # Create token
-    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    access_token = oauth2.create_access_token(data={"user_id": str(user.id)})
 
     # Return Token
     return {"access_token": access_token, "token_type": "bearer"}
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
 from typing import List
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from ..database import get_db
 
 router = APIRouter(
@@ -11,12 +11,13 @@ router = APIRouter(
 
 # post/create request
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
-async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published)
     #                VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # connection.commit()
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    print(current_user.email)
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
     db.commit()
@@ -25,7 +26,7 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 # get all posts
 @router.get("/", response_model=List[schemas.PostResponse])
-async def get_posts(db: Session = Depends(get_db)):
+async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
@@ -33,7 +34,7 @@ async def get_posts(db: Session = Depends(get_db)):
 
 # get individual post
 @router.get("/{id}", response_model=schemas.PostResponse)
-async def get_post(id: int, db: Session = Depends(get_db)):
+async def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,))
     # post = cursor.fetchone()
     
@@ -46,7 +47,7 @@ async def get_post(id: int, db: Session = Depends(get_db)):
 
 # deleting
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int, db: Session = Depends(get_db)):
+async def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
     # deleted_post = cursor.fetchone()
     # connection.commit()
@@ -64,7 +65,7 @@ async def delete_post(id: int, db: Session = Depends(get_db)):
 
 # updating
 @router.put("/{id}", response_model=schemas.PostResponse)
-async def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+async def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s
     #                   WHERE id = %s RETURNING *""",
     #                (post.title, post.content, post.published, id))
